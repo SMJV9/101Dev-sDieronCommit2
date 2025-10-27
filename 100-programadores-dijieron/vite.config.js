348d90ec82a666fef824b3158cf7b92470ec986a
SMJV9 <a22311022@uthermosillo.edu.mx>
2025-10-27 16:49:59 -0700

    Se cambio el organizo el css y el html, se actualizo el proyecto para usar vite

--- 100-programadores-dijieron/vite.config.js
+++ 100-programadores-dijieron/vite.config.js
@@ -5,7 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/controller.css',
+                'resources/css/board.css',
+                'resources/css/questions.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
